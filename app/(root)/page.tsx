3999a5209ce753423a21f895d727fb263a06761d
DJ-Weiman <dionweiman1998@gmail.com>
2025-07-01 22:28:52 +0530

    Minor Fix for Book Pagination

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,8 +4,10 @@ import { getBooks } from '@/lib/books'
 import React from 'react'
 
 
-const Home = async () => {
-  const {books, numberOfPages} = await getBooks(1, 10)
+const Home = async (props: { searchParams?: Promise<{ page?: string }> }) => {
+  const searchParams = await props.searchParams
+  const currentPage = Number(searchParams?.page) || 1;
+  const { books, numberOfPages } = await getBooks(currentPage, 10)
 
   return (
     <div className='flex flex-col items-center'>
--- components/BookItem.tsx
+++ components/BookItem.tsx
@@ -14,12 +14,21 @@ function BookItem({ id, coverUrl, title, genre }: Props) {
     <Link href={`/books/${id}`}>
       <div className="relative max-w-40">
         <div className="relative w-40 h-50">
-          <Image
-            src='/icons/book.svg'
-            alt="Book Cover"
-            fill
-            className="rounded-sm object-cover invert"
-          />
+          {
+            coverUrl ?
+              <Image
+                src={coverUrl}
+                alt="Book Cover"
+                fill
+                className="rounded-sm object-cover"
+              /> :
+              <Image
+                src='/icons/book.svg'
+                alt="Book Cover"
+                fill
+                className="rounded-sm object-cover invert"
+              />
+          }
         </div>
 
         <div className="flex flex-col gap-2 mt-2">
